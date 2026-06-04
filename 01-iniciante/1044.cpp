1b4e2943880b8ded477d9ab5803ec2122a50651c
Arthur Ramalho <artramalho@proton.me>
2026-06-03 22:55:18 -0300

    estruturação inicial do repositório

---
+++ 01-iniciante/1044.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main () {
+       ios::sync_with_stdio(false);
+       cin.tie(nullptr);
+       int a, b;
+       cin >> a >> b;
+       if(a > b) {
+              if (a%b == 0) {
+                     cout << "Sao Multiplos" << "\n";
+              }
+              else {
+                     cout << "Nao sao Multiplos" << "\n";
+              }
+       }
+       else {
+             if (b%a == 0) {
+                     cout << "Sao Multiplos" << "\n";
+              }
+              else {
+                     cout << "Nao sao Multiplos" << "\n";
+              } 
+       }
+       return 0;
+}
---
+++ 04-estruturas-e-bibliotecas/1259.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main () {
+       ios::sync_with_stdio(false);
+       cin.tie(nullptr);
+       int n;
+       cin >> n;
+       vector<int> pares;
+       vector<int> impares;
+       for(int i = 0; i < n; i++) {
+              int num;
+              cin >> num;
+              if(num % 2 == 0) {
+                     pares.push_back(num);
+              }
+              else {
+                     impares.push_back(num);
+              }
+       }
+       sort(pares.begin(), pares.end());
+       sort(impares.begin(), impares.end());
+       reverse(impares.begin(), impares.end());
+       pares.insert(pares.end(), impares.begin(), impares.end());
+       for (int i = 0; i < pares.size(); i++) {
+              cout << pares[i] << "\n";
+       }
+}
