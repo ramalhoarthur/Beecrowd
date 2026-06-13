fb3ab5f37eff19aad3d0c34d3d49ac15d2a08f8c
Arthur Ramalho <artramalho@proton.me>
2026-06-12 22:09:29 -0300

    feat: Adiciona template inicial para codigo c++

---
+++ template.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std; 
+
+typedef long long ll;
+typedef vector<int> vi;
+
+int main () {
+       ios::sync_with_stdio(false);
+       cin.tie(nullptr);
+       return 0;
+}
