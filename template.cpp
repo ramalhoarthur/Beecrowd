3f2041b3c1b197b48412123a86ed62393938106d
Arthur Ramalho <artramalho@proton.me>
2026-06-30 18:09:17 -0300

    fix: Atualização do template

--- template.cpp
+++ template.cpp
@@ -1,3 +1,6 @@
+//Nome
+//https://judge.beecrowd.com/pt/problems/view/numero_do_problema
+
 #include <bits/stdc++.h>
 using namespace std; 
 
@@ -5,14 +8,15 @@ typedef long long ll;
 typedef vector<int> vi;
 
 int main () {
+
        ios::sync_with_stdio(false);
        cin.tie(nullptr);
+
        int t; cin >> t;
+
        while(t--) {
               
        }
+       
        return 0;
 }
-
-//Nome
-//https://judge.beecrowd.com/pt/problems/view/numero_do_problema
